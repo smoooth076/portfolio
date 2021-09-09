ebb97a37e51e38bfa40aad7b4d25bc40665b20e5
smoooth076 <86213227+smoooth076@users.noreply.github.com>
2021-09-09 18:33:16 +0900

    이메일 폼 업데이트

--- js/portfolio.js
+++ js/portfolio.js
@@ -29,4 +29,17 @@ $(function() {
             $(this).addClass('on');
         });
     });
+    document.querySelector('#contactForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // submit이벤트 막기
+        const fromName = document.querySelector('input[name="from_name"]').value; // 전송자 이름 추출
+
+        emailjs.init("user_JjSS0asucepLDFpyXulKc"); // API keys
+        emailjs.sendForm('service_p40jtvn', 'template_5xnegrj', this)
+            .then(function() {
+                alert(`${fromName}님, 메일 전송 완료 되었습니다 :)`)
+            }, function(error) {
+                alert(`${fromName}님, 메일 전송이 실패했습니다 :(`)
+                console.log('전송실패', error);
+            });
+    });
 });
