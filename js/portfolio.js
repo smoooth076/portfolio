bb0884ff00a1266a16a8cbae77032d2f78772d68
smoooth076 <smoooth076@gmail.com>
2021-09-15 10:40:41 +0900

    ie 수정

--- js/portfolio.js
+++ js/portfolio.js
@@ -41,14 +41,16 @@ $(function() {
     });
     document.querySelector('#contactForm').addEventListener('submit', function(event) {
         event.preventDefault(); // submit이벤트 막기
-        const fromName = document.querySelector('input[name="from_name"]').value; // 전송자 이름 추출
+        var fromName = document.querySelector('input[name="from_name"]').value; // 전송자 이름 추출
+        var sendSuccess = '메일 전송에 성공했습니다.';
+        var sendFail = '메일 전송에 실패했습니다.';
 
         emailjs.init("user_JjSS0asucepLDFpyXulKc"); // API keys
         emailjs.sendForm('service_p40jtvn', 'template_5xnegrj', this)
             .then(function() {
-                alert(`${fromName}님, 메일 전송 완료 되었습니다 :)`)
+                alert(sendSuccess);
             }, function(error) {
-                alert(`${fromName}님, 메일 전송이 실패했습니다 :(`)
+                alert(sendFail);
                 console.log('전송실패', error);
             });
     });
