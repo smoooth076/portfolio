4aaf17a67131e89097fad907c76e694912777b87
smoooth076 <86213227+smoooth076@users.noreply.github.com>
2024-08-27 17:55:23 +0900

    desc 추가

--- asset/js/script.js
+++ asset/js/script.js
@@ -5,11 +5,18 @@ $(function () { // wait for document ready
       scrollTop : 0
     }, 500);
   });
+  $(".sec02 .desc_wrap button").click(function() {
+    $(this).siblings().slideUp(400);
+    $(this).removeClass("on");
+    $(".sec02 ul.thumb li").removeClass("active");
+  });
 
   $(".sec02 ul.thumb li").click(function() {
     $(this).addClass("active").siblings().removeClass("active");
     $(this).parent().siblings().find("li").removeClass("active")
     $(".sec02 .desc").css("display","none");
-    $("#" + $(this).data('id')).slideDown(400).siblings().css("display","none");
+    $("#" + $(this).data('id')).slideDown(400).siblings("div").css("display","none");
+    $("#" + $(this).data('id')).siblings("button").addClass("on");
   });
+  
 });
