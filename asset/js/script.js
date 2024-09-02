c5350ebbda7f1f8bc34d066225ca239675616d43
smoooth076 <86213227+smoooth076@users.noreply.github.com>
2024-09-02 17:00:44 +0900

    반응형 추가

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,22 +1,32 @@
 $(function () { // wait for document ready
+
   AOS.init();
+
   $(".flow-text").on("click", function(){
     $("html, body").animate({
       scrollTop : 0
     }, 500);
   });
+
   $(".sec02 .desc_wrap button").click(function() {
-    $(this).siblings().slideUp(400);
-    $(this).removeClass("on");
+    const $this = $(this);
+    $this.siblings().slideUp(400);
+    $this.removeClass("on");
     $(".sec02 ul.thumb li").removeClass("active");
   });
 
   $(".sec02 ul.thumb li").click(function() {
-    $(this).addClass("active").siblings().removeClass("active");
-    $(this).parent().siblings().find("li").removeClass("active")
-    $(".sec02 .desc").css("display","none");
-    $("#" + $(this).data('id')).slideDown(400).siblings("div").css("display","none");
-    $("#" + $(this).data('id')).siblings("button").addClass("on");
+    const $this = $(this);
+    const targetId = $this.data('id');
+    
+    $this.addClass("active").siblings().removeClass("active");
+    $this.parents(".thumb").siblings().find("li").removeClass("active")
+    $(".sec02 .desc").hide();
+    $("#" + targetId).slideDown(400).siblings("div").hide();
+    
+    const $descWrap = $("#" + targetId).parents(".desc_wrap");
+    $descWrap.siblings().find("button").removeClass("on");
+    $descWrap.find("button").addClass("on");
   });
   
 });
